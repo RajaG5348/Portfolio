3d984fb20668fe2a30e5dfd18b68ebca0a25be30
RajaG5348 <mr.raja.g12@gmail.com>
2024-11-04 17:41:05 +0530

    media query updated

--- script.js
+++ script.js
@@ -1,38 +1,36 @@
-const toggle = document.getElementById('nav_toggle'),
-      nav = document.getElementById('nav_menu')
+const toggle = document.getElementById("nav_toggle"),
+  nav = document.getElementById("nav_menu");
 
-   
-    // show menu 
+// show menu
 
-    const showMenu = ()=>{
-        if(toggle && nav){
-            toggle.addEventListener("click", ()=>{
-                nav.classList.toggle('show')
-            })
-        }
-    }
-    showMenu();
-
-    // remove Menu
-const removeMenu = ()=>{
-    const navLink = document.querySelectorAll('.nav_link') 
-
-
-    navLink.forEach(element => {
-          element.addEventListener("click", ()=>{
-              nav.classList.remove("show")
-          })    
+const showMenu = () => {
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show");
     });
-}
+  }
+};
+showMenu();
+
+// remove Menu
+const removeMenu = () => {
+  const navLink = document.querySelectorAll(".nav_link");
+
+  navLink.forEach((element) => {
+    element.addEventListener("click", () => {
+      nav.classList.remove("show");
+    });
+  });
+};
 
 removeMenu();
 
 // open webpage
 
-const openUrl = ()=>{
-    const openUrl = document.getElementById('openUrl')
-    openUrl.addEventListener('click',()=>{
-        window.open('https://rajag5348.github.io/Food-delivery-webpage/')
-    } )
-}
-openUrl()
+const openUrl = () => {
+  const openUrl = document.getElementById("openUrl");
+  openUrl.addEventListener("click", () => {
+    window.open("https://rajag5348.github.io/Food-delivery-webpage/");
+  });
+};
+openUrl();
